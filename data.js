10811c605889c9401de552899c2200495925de4c
kirnotes <ramgerninochea@gmail.com>
2026-03-13 05:48:46 -0400

    Update data.js

--- data.js
+++ data.js
@@ -743,7 +743,139 @@ const KEYWORD_HINTS_RAW = {
 };
 
 export const KEYWORD_HINTS = cleanHintMap(KEYWORD_HINTS_RAW);
+export const WRAP_DETAILS = {
+  "Account - 1st Box Change / Question": `Issues, questions, changes, and cancellation of the 1st box. This code should be applied only if the request was made before the deadline.`,
 
+  "Account - After deadline change request": `Inquiries related to after-deadline changes or box pauses. Includes after-deadline 1st box cancellation request.`,
+
+  "Account - Cancellation - Duplicated Subscription": `Customer requests cancellation of subscription due to a duplicate subscription.`,
+
+  "Account - Cancellation - Customer Not Retained": `The customer requested subscription cancellation, and the agent was not able to prevent the customer from canceling.
+
+Note:
+* For before-deadline box cancellation, please use "Account- Skip/Unskip"
+* For 1st box before the deadline cancellation, please use "Account - 1st box change/Question".
+* For after-deadline box cancellation, please use "Account - After deadline change request".`,
+
+  "Account - Cancellation - Customer Retained": `The customer requested subscription cancellation, and the agent prevented the customer from canceling the subscription.
+
+Note:
+* For before-deadline box cancellation, please use "Account- Skip/Unskip".
+* For 1st box before the deadline cancellation, please use "Account - 1st box change/Question".
+* For after-deadline box cancellation, please use "Account - After deadline change request".`,
+
+  "Account - Data Protection": `Questions or requests related to data protection, such as deleting personal data or obtaining a copy of personal information.`,
+
+  "Account - Loyalty Program": `Interactions involving the HelloFresh+ Loyalty Program. Within the program, the customer unlocks milestones and rewards based on their order count. Customers need to opt in to the program, meaning they need to actively sign up to start collecting rewards.`,
+
+  "Account - Meal Swap / Recipe Preference": `Inquiries about recipes and their dietary requirements, meal and add-ons swaps.`,
+
+  "Account - Reactivation": `Queries related to account reactivation.`,
+
+  "Account - Skip/Unskip": `Pause/unpause box requests. This code should be used for BOX cancellation (except 1st box) requested before the deadline. In this case, the customer will continue with their subscription.
+
+Note:
+* For after-deadline box cancellation, please use "Account - After deadline change request".`,
+
+  "Account -Update account / subscription details": `Inquiries related to personal and subscription information changes before the deadline, i.e. delivery day, address, edit phone number, edit email address, payment method change, meal size changes, password reset. This also includes issues related to account access, i.e. customer cannot access their accounts.
+
+General inquiry about RTE/Factor meals.`,
+
+  "Complaint - Food Safety": `Food safety-related complaints such as allergic reaction, foreign objects found in products, and food poisoning/foodborne illness.
+Raw or undercooked protein (only applicable to RTE and The Pets Table)
+
+Possible reasons for contact:
+* Food Safety Recalls
+* Food Safety Escalation`,
+
+  "Complaint - Quality": `Complaints on the ingredients quality and packaging issues, i.e. damaged, poor quality/value perception, issue with temperature, damaged external packaging, etc.`,
+
+  "Complaint - Pick & Pack": `Complaints related to pick and pack issues, i.e. incorrect, missing, or extra ingredients or meal kits.`,
+
+  "Complaint - Website / App / Internal Tech Error": `Complaints on the app/website/internal technical issues, including changes made not applied, glitches, system outages, etc.`,
+
+  "Logistics - Delivery status": `Inquiries related to delivery status and ETA requests within the delivery window.`,
+
+  "Complaint - Logistic - Courier company": `Complaints due to property damaged by logistics company, driver behavior, delivery instructions not followed, leave safe, etc.
+
+Possible reasons for contact:
+* Damaged boxes by driver or courier
+* HFDN logistics issue (not including causes resulting in no delivery)`,
+
+  "Complaint - Logistics - Early/Late": `Complaints due to delivery before or after the advised time slot/delivery window (NOT the ETA provided by the courier). Applicable when the box was delivered after 1 to 2 days of the expected delivery date.
+
+For US only: Please consider that The Pets Table and Factor Form only have 4+ days late.`,
+
+  "Complaint - Logistics - Failed delivery": `Complaints due to any cause resulting in no delivery.
+
+Possible reasons for contact:
+* Request for replacement or remake
+* HFDN logistics issue resulting in no delivery`,
+
+  "Marketing - Applying Voucher / Gift Card": `Inquiries concerning vouchers and codes, such as expired or codes not applied to orders, and escalations.
+
+Possible reasons for contact:
+* Loyalty Reward
+* Reactivation Code`,
+
+  "Marketing - Communication": `Inquiries related to any comms about our marketing offers, collaboration requests, product endorsement proposals, unsubscribing from comms, partnership requests, advertisement, etc.
+
+Possible reasons for contact:
+* Free Meals Promotion
+* Percentage Discount`,
+
+  "Marketing - Direct Sales and Reactivation team": `Issues related to any direct sales agents, door-to-door, outbound call center, and reactivation teams.
+
+Possible reasons for contact:
+* Direct sales behavior`,
+
+  "Marketing - Referral": `Any referral issues including policy, referral credit, Helloshare code, etc.`,
+
+  "Marketing - Voucher Fraud": `Interactions involving voucher(s)/freebie fraud.`,
+
+  "Other - Connection Issue": `Connection issues experienced by CC agents, i.e. customer can't see the agents' answers, and calls are disconnected.
+
+Wrap code to be used only when the query/issue is not yet known.`,
+
+  "Other - Non-customer query": `Non-customers enquiries, i.e.:
+* Courier proposal, event invites, etc
+* Requests about POCs in the company
+* Applications
+* Passed on details (BBB-Better Business Bureau), etc.
+
+Note: Refer to market guidelines as to what are identified as general inquiries.`,
+
+  "Other - Sustainability / Sourcing": `Inquiries about resources, suppliers, alternatives, packaging, recycling, ice packs and ice pack disposal.`,
+
+  "US ONLY: CISP Interaction": `Interactions with customers who are using hostile or derogatory language toward agents.`,
+
+  "Payments - Charge breakdown": `Charge breakdown queries.
+
+Possible reasons for contact:
+* Add-on / Extras
+* Amazon Prime
+* Confirmed payment
+* Delivery fee
+* Double charge
+* Incorrect charge
+* Invoice
+* Price change
+* Regional fee
+* Standard shipping
+* Unaware of surcharge
+* Unauthorised charge`,
+
+  "Payments - Credit/ Refund": `Inquiries on credit/refund.
+
+Possible reasons for contact:
+* HelloFresh Cash
+* Credit balance/confirmation
+* Customer received credit originally and now would prefer a refund`,
+
+  "Payments - Outstanding / Dunning": `Queries from customers that have outstanding payments or are in dunning.
+
+For US: This includes Unpaid Shipped Boxes.`
+};
 export const NIA_LIBRARY = {
   "Account - Cancellation - Customer Not Retained": {
     "Can't Afford / Out of Budget": {
@@ -1255,3 +1387,4 @@ If you have both a discount code and a credit in your account, we'll apply the d
     }
   }
 };
+
